6eda0d48c632e2921a1dff65dfd1b025c1b41689
nayyaung9 <naygit.developer@gmail.com>
2021-05-31 13:39:34 +0630

    fix: remove length 1 in product

--- components/products/ProductGridList.js
+++ components/products/ProductGridList.js
@@ -6,12 +6,11 @@ const ProductGridList = ({ products, productListName }) => {
   return (
     <React.Fragment>
       <Heading as="h4" color="gray.600" size="md" mb="3">
-        {productListName ? productListName : 'Just For You'}
+        {productListName ? productListName : "Just For You"}
       </Heading>
       <SimpleGrid columns={2} minChildWidth="250px" spacing="10px">
         {products && products.length === 0 && <div>No Products</div>}
         {Array.isArray(products) &&
-          products.length > 1 &&
           products.map((product, i) => (
             <ProductItemCard product={product} key={i} />
           ))}
